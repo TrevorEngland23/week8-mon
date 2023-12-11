b8f81815b5b9b8f3d644029ea9c9565a682d83ce
TrevorEngland23 <trevor.england.523@gmail.com>
2023-12-11 14:09:34 -0800

    test 3 complete

--- Main.java
+++ Main.java
@@ -7,6 +7,9 @@ public class Main {
         
     }
     public static boolean isPrime(int number){
+        if(number == 1){ // edge case
+            return false;
+        }
         for(int i =2; i <= Math.sqrt(number); i++){
             if(number % i == 0) {
                 return false; // a divisor is found
--- MainTest.java
+++ MainTest.java
@@ -6,26 +6,27 @@ public class MainTest {
     @Test
     public void testIsPrimeTrue() {
         // Arrange
-
         // Act
         boolean output = Main.isPrime(5); // include name of class
-
         // Assert
-
         assertTrue(output); 
     }
 
     @Test
-
     public void testIsPrimeFalse() {
         // Arrange
-
         // Act
-
         boolean output = Main.isPrime(4);
-
         // Assert
+        assertFalse(output);
+    }
 
+    @Test
+    public void testIsPrimeNumberOne(){
+        //Arrange
+        //Act
+        boolean output = Main.isPrime(1);
+        //Assert
         assertFalse(output);
     }
 }
